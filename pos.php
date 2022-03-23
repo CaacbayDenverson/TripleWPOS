9bfaef9160173c23b4aba70b8a548762548ff9b9
DenversonCaacbay <denverkunfalcon@gmail.com>
2022-03-23 15:19:08 +0800

    POS

--- pos.php
+++ pos.php
@@ -80,6 +80,248 @@
           <div class="text_permission">Point of Sale</div>
           <br>
       </section>
+      <section class="inventory-section">
+          <div class="text_permission">
+        
+    <!-- EDIT POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Edit Product Info </h5>
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" aria-label="Close">
+                        x
+                    </button>
+                </div>
+
+                <form action="sql/inventory_update.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="hidden" name="product_id" id="product_id">
+
+                        <div class="form-group">
+                            <label> Product Name </label>
+                            <input type="text" name="product_name" id="product_name" class="form-control"
+                                placeholder="Enter New Product Name">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Price </label>
+                            <input type="text" name="product_price" id="product_price" class="form-control"
+                                placeholder="Enter New Price">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Quantity </label>
+                            <input type="text" name="product_qty" id="product_qty" class="form-control"
+                                placeholder="Enter New Quantity">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" name="updatedata" class="btn btn-primary">Update Data</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- DELETE POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="deletemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Delete Product Data </h5>
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" aria-label="Close">
+                        x
+                    </button>
+                </div>
+
+                <form action="sql/inventory_delete.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="hidden" name="product_delete" id="delete_id">
+
+                        <h4> Do you want to Delete this product?</h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal"> NO </button>
+                        <button type="submit" name="deletedata" class="btn btn-primary"> YES</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- VIEW POP UP FORM (Bootstrap MODAL) -->
+    <div class="modal fade" id="viewmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> View Student Data </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="deletecode.php" method="POST">
+
+                    <div class="modal-body">
+
+                        <input type="text" name="view_id" id="view_id">
+
+                        <!-- <p id="fname"> </p> -->
+                        <h4 id="fname"> <?php echo ''; ?> </h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal"> CLOSE </button>
+                        <!-- <button type="submit" name="deletedata" class="btn btn-primary"> Yes !! Delete it. </button> -->
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="">
+        <div class="jumbotron">
+                <div class="card-body">
+                <form action="inventory.php" method="post">
+            <div class="sub-btn">
+                <input type="text" style="width:40%" name="search" placeholder="Select Product">
+                <input type="number" style="width:5%" name="search" placeholder="1">
+
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#studentaddmodal">
+                        Add Product
+                    </button>
+            </div>
+                    
+                </div>
+                    <table style="width:100%;" id="datatableid" class="table table-bordered table-dark">
+                        
+                            <tr>
+                                <th> ID</th>
+                                <!-- <th>Image</th> -->
+                                <th>Product Name </th>
+                                <th> Price </th>
+                                <!-- <th> Marketed By </th>
+                                <th>Category</th> -->
+                                <th> Quantity </th>
+                                <th> Total  </th>
+                                <th> DELETE </th>
+                            </tr>
+
+                           
+                    </form>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+
+            $('.viewbtn').on('click', function () {
+                $('#viewmodal').modal('show');
+                $.ajax({ //create an ajax request to display.php
+                    type: "GET",
+                    url: "display.php",
+                    dataType: "html", //expect html to be returned                
+                    success: function (response) {
+                        $("#responsecontainer").html(response);
+                        //alert(response);
+                    }
+                });
+            });
+
+        });
+    </script>
+
+
+    <!-- <script>
+        $(document).ready(function () {
+
+            $('#datatableid').DataTable({
+                "pagingType": "full_numbers",
+                "lengthMenu": [
+                    [10, 25, 50, -1],
+                    [10, 25, 50, "All"]
+                ],
+                responsive: true,
+                language: {
+                    search: "_INPUT_",
+                    searchPlaceholder: "Search Your Data",
+                }
+            });
+
+        });
+    </script> -->
+
+    <script>
+        $(document).ready(function () {
+
+            $('.deletebtn').on('click', function () {
+
+                $('#deletemodal').modal('show');
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#delete_id').val(data[0]);
+
+            });
+        });
+    </script>
+
+    <script>
+        $(document).ready(function () {
+
+            $('.editbtn').on('click', function () {
+
+                $('#editmodal').modal('show');
+
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function () {
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+
+                $('#product_id').val(data[0]);
+                $('#product_name').val(data[1]);
+                $('#product_price').val(data[2]);
+                $('#product_qty').val(data[3]);
+            });
+        });
+    </script>
+        </div>
+      </section>
     
       <script src="js/script.js"></script>
     
