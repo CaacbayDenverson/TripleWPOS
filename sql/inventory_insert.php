819f2e7e3f080436e33e05ba5aa8c597290fa411
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2022-05-22 15:12:00 +0800

    Update inventory_insert.php

--- sql/inventory_insert.php
+++ sql/inventory_insert.php
@@ -32,36 +32,35 @@
             </script>";
         }
         else{
-            echo "proceed";
+            // insert product
+            $sql = 'INSERT INTO product(product_name, code,product_price, product_qty) VALUES (:product_name, :product_code,:product_price, :product_qty)';
+
+            $statement = $pdo->prepare($sql);
+
+            $newProd = [
+                'product_name' => 'placeholder',
+                'product_code' => 'zzz',
+                'product_price' => 999,
+                'product_qty' => 999,
+            ];
+
+            $statement->bindParam(':product_name', $newProd['product_name']);
+            $statement->bindParam(':product_code', $newProd['product_code']);
+            $statement->bindParam(':product_price', $newProd['product_price']);
+            $statement->bindParam(':product_qty', $newProd['product_qty']);
+
+            //change
+            $newProd['product_name'] = $_POST['productNew_name'];
+            $newProd['product_code'] = $_POST['productNew_code'];
+            $newProd['product_price'] = $_POST['productNew_price'];
+            $newProd['product_qty'] = $_POST['productNew_qty'];
+
+            //execute query
+            $statement->execute();
+            header("Location: ../inventory.php");
+            exit();
         }
 
     }
 
-    // // insert product
-    // $sql = 'INSERT INTO product(product_name, code,product_price, product_qty) VALUES (:product_name, :product_code,:product_price, :product_qty)';
-
-    // $statement = $pdo->prepare($sql);
-
-    // $newProd = [
-    //     'product_name' => 'placeholder',
-    //     'product_code' => 'zzz',
-    //     'product_price' => 999,
-    //     'product_qty' => 999,
-    // ];
-
-    // $statement->bindParam(':product_name', $newProd['product_name']);
-    // $statement->bindParam(':product_code', $newProd['product_code']);
-    // $statement->bindParam(':product_price', $newProd['product_price']);
-    // $statement->bindParam(':product_qty', $newProd['product_qty']);
-
-    // //change
-    // $newProd['product_name'] = $_POST['productNew_name'];
-    // $newProd['product_code'] = $_POST['productNew_code'];
-    // $newProd['product_price'] = $_POST['productNew_price'];
-    // $newProd['product_qty'] = $_POST['productNew_qty'];
-
-    // //execute query
-    // $statement->execute();
-    // header("Location: ../inventory.php");
-    // exit();
 ?>
